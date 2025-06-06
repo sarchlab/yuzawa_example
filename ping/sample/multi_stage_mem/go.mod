7c70bbc8645b26020605786527905b3b4340b0ce
Sabila Al Jannat <sajannat@wm.edu>
2025-06-06 07:08:55 -0400

    multi_stage_mem

---
+++ ping/sample/multi_stage_mem/go.mod
@@ -0,0 +1,28 @@
+module main
+
+go 1.24.0
+
+require (
+	github.com/sarchlab/akita/v4 v4.4.0
+	github.com/sarchlab/yuzawa_example v0.0.0-20250512131650-b97cc6efabfb
+)
+
+require (
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/google/pprof v0.0.0-20240829160300-da1f7e9f2b25 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/rs/xid v1.6.0 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/syifan/goseth v0.1.2 // indirect
+	github.com/tebeka/atexit v0.3.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.8.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
+
+replace (
+	github.com/sarchlab/akita/v4 v4.4.0 => /Users/sabilaaljannat/akita
+	github.com/sarchlab/yuzawa_example => /Users/sabilaaljannat/yuzawa_example
+)
---
+++ ping/sample/multi_stage_mem/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/sarchlab/akita/v4/mem/cache/writeback"
+	"github.com/sarchlab/akita/v4/mem/cache/writethrough"
+	"github.com/sarchlab/akita/v4/mem/idealmemcontroller"
+	"github.com/sarchlab/akita/v4/mem/mem"
+	"github.com/sarchlab/akita/v4/mem/trace"
+	"github.com/sarchlab/akita/v4/mem/vm/addresstranslator"
+	"github.com/sarchlab/akita/v4/sim"
+	"github.com/sarchlab/akita/v4/simulation"
+	"github.com/sarchlab/akita/v4/mem/vm/tlb"
+	"github.com/sarchlab/akita/v4/sim/directconnection"
+	"github.com/sarchlab/akita/v4/tracing"
+	"github.com/sarchlab/yuzawa_example/ping/memaccessagent"
+	"github.com/sarchlab/yuzawa_example/ping/rob"
+	"github.com/sarchlab/yuzawa_example/ping/benchmarks/multi_stage_memory"
+	"github.com/sarchlab/yuzawa_example/ping/mmu"
+)
+
+func main() {
+	// simulation := sim.NewSimulation()
+	// engine := sim.NewParallelEngine()
+	// simulation.RegisterEngine(engine)
+
+	simBuilder := simulation.MakeBuilder().Build()
+	// simulation := simBuilder.Build()
+	engine := simBuilder.GetEngine()
+
+	MemCtrl := idealmemcontroller.MakeBuilder().
+		WithEngine(engine).
+		WithNewStorage(4 * mem.GB).
+		WithLatency(100).
+		Build("MemCtrl")
+	simBuilder.RegisterComponent(MemCtrl)
+
+	L2Cache := writeback.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithWayAssociativity(4).
+		WithNumReqPerCycle(2).
+		WithAddressMapperType("single").
+		WithRemotePorts(MemCtrl.GetPortByName("Top").AsRemote()).
+		Build("L2Cache")
+	simBuilder.RegisterComponent(L2Cache)
+
+	L1Cache := writethrough.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithWayAssociativity(2).
+		WithAddressMapperType("single").
+		WithRemotePorts(L2Cache.GetPortByName("Top").AsRemote()).
+		Build("L1Cache")
+	simBuilder.RegisterComponent(L1Cache)
+
+	IoMMU := mmu.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithLog2PageSize(12).
+		WithMaxNumReqInFlight(16).
+		WithPageWalkingLatency(10).
+		Build("IoMMU")
+	simBuilder.RegisterComponent(IoMMU)
+
+	L2TLB := tlb.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithNumWays(64).
+		WithNumSets(64).
+		WithPageSize(4096).
+		WithNumReqPerCycle(4).
+		WithRemotePorts(IoMMU.GetPortByName("Top").AsRemote()).
+		WithAddressMapperType("single").
+		Build("L2TLB")
+	simBuilder.RegisterComponent(L2TLB)
+
+	TLB := tlb.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithNumWays(8).
+		WithNumSets(8).
+		WithPageSize(4096).
+		WithNumReqPerCycle(2).
+		WithRemotePorts(L2TLB.GetPortByName("Top").AsRemote()).
+		WithAddressMapperType("single").
+		Build("TLB")
+	simBuilder.RegisterComponent(TLB)
+
+	AT := addresstranslator.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithLog2PageSize(12).
+		WithTranslationProvider(TLB.GetPortByName("Top").AsRemote()).
+		WithRemotePorts(L1Cache.GetPortByName("Top").AsRemote()).
+		WithAddressMapperType("single").
+		Build("AT")
+	simBuilder.RegisterComponent(AT)
+
+	ROB := rob.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithNumReqPerCycle(4).
+		WithBufferSize(128).
+		Build("ROB")
+	simBuilder.RegisterComponent(ROB)
+
+	ROB.BottomUnit = AT.GetPortByName("Top")
+	if ROB.BottomUnit == nil {
+		panic("Failed to assign BottomUnit: Top port not found")
+	}
+
+	MemAgent := memaccessagent.MakeBuilder().
+		WithFreq(1 * sim.GHz).
+		WithMaxAddress(1 * mem.GB).
+		WithWriteLeft(100000).
+		WithReadLeft(100000).
+		WithEngine(engine).
+		Build("MemAgent")
+	simBuilder.RegisterComponent(MemAgent)
+	MemAgent.LowModule = ROB.GetPortByName("Top")
+	if MemAgent.LowModule == nil {
+		panic("Failed to assign LowModule: Top port not found")
+	}
+	
+	Conn1 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn1")
+	Conn1.PlugIn(MemAgent.GetPortByName("Mem"))
+	Conn1.PlugIn(ROB.GetPortByName("Top"))
+
+	Conn2 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn2")
+	Conn2.PlugIn(ROB.GetPortByName("Bottom"))
+	Conn2.PlugIn(AT.GetPortByName("Top"))
+
+	Conn3 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn3")
+	Conn3.PlugIn(AT.GetPortByName("Translation"))
+	Conn3.PlugIn(TLB.GetPortByName("Top"))
+
+	Conn4 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn4")
+	Conn4.PlugIn(TLB.GetPortByName("Bottom"))
+	Conn4.PlugIn(L2TLB.GetPortByName("Top"))
+
+	Conn5 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn5")
+	Conn5.PlugIn(L2TLB.GetPortByName("Bottom"))
+	Conn5.PlugIn(IoMMU.GetPortByName("Top"))
+
+	Conn6 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn9")
+	Conn6.PlugIn(AT.GetPortByName("Bottom"))
+	Conn6.PlugIn(L1Cache.GetPortByName("Top"))
+
+	Conn7 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn10")
+	Conn7.PlugIn(L1Cache.GetPortByName("Bottom"))
+	Conn7.PlugIn(L2Cache.GetPortByName("Top"))
+
+	Conn8 := directconnection.MakeBuilder().WithEngine(engine).WithFreq(1 * sim.GHz).Build("Conn11")
+	Conn8.PlugIn(L2Cache.GetPortByName("Bottom"))
+	Conn8.PlugIn(MemCtrl.GetPortByName("Top"))
+
+	traceFile, err := os.Create("trace.log")
+	if err != nil {
+		panic("Error: Failed to create trace file")
+	}
+	logger := log.New(traceFile, "", 0)
+	tracer := trace.NewTracer(logger, engine)
+	tracing.CollectTrace(MemCtrl, tracer)
+
+	benchmark := multi_stage_memory.MakeBuilder().
+	WithSimulation(simBuilder).
+	WithNumAccess(100000).
+	WithMaxAddress(1 * mem.GB).
+	Build("Benchmark")
+	benchmark.Run()
+}
